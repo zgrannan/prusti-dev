8d3b393a77426c712bcca9fdab6c2753b3f3435a
Zack Grannan <zgrannan@protonmail.com>
2021-12-15 08:17:53 -0800

    restore some old code

--- prusti-specs/src/lib.rs
+++ prusti-specs/src/lib.rs
@@ -445,39 +445,9 @@ pub fn refine_trait_spec(_attr: TokenStream, tokens: TokenStream) -> TokenStream
     }
 }
 
-pub fn invariant(attr: TokenStream, tokens:TokenStream) -> TokenStream {
-    let mut rewriter = rewriter::AstRewriter::new();
-    let spec_id = rewriter.generate_spec_id();
-    let invariant = handle_result!(rewriter.parse_assertion(spec_id, attr));
-    let item: syn::DeriveInput = handle_result!(syn::parse2(tokens));
-    let item_span = item.span();
-    let item_ident = item.ident.clone();
-    let item_name = syn::Ident::new(
-        &format!("prusti_struct_invariant_item_{}_{}", item_ident.to_string().to_lowercase(), spec_id),
-        item_span,
-    );
-    let mut statements = TokenStream::new();
-    invariant.encode_type_check(&mut statements);
-    let spec_id_str = spec_id.to_string();
-    let assertion_json = crate::specifications::json::to_json_string(&invariant);
-
-    let spec_item: syn::ItemFn = parse_quote_spanned! {item_span=>
-        #[allow(unused_must_use, unused_variables, dead_code)]
-        #[prusti::spec_only]
-        #[prusti::spec_id = #spec_id_str]
-        #[prusti::assertion = #assertion_json]
-        fn #item_name(self) {
-            #statements
-        }
-    };
-    // spec_item.sig.generics = item.sig().generics.clone();
-    // spec_item.sig.inputs = item.sig().inputs.clone();
-    quote_spanned! { item_span =>
-       #item
-       impl #item_ident {
-          #spec_item
-       }
-    }
+#[proc_macro_attribute]
+pub fn invariant(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
+    tokens
 }
 
 pub fn extern_spec(_attr: TokenStream, tokens:TokenStream) -> TokenStream {
