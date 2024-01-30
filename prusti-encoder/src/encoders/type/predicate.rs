d25d11ca9f8ae37fb950441d39ec7368f3ef23e0
Zack Grannan <zgrannan@protonmail.com>
2024-01-29 19:23:21 -0800

    cleanup

--- prusti-encoder/src/encoders/type/predicate.rs
+++ prusti-encoder/src/encoders/type/predicate.rs
@@ -16,39 +16,26 @@ pub struct PredicateEncOutputRef<'vir> {
     /// The predicate output for the "most generic version" of the input type
     pub generic_predicate: GenericPredicateEncOutputRef<'vir>,
 
-    /// The lifted representation of the input type as a Viper value
+    /// The lifted representation of the input type, as a Viper value
     pub ty: LiftedTy<'vir>,
 }
 
 impl<'vir> PredicateEncOutputRef<'vir> {
-
-    pub fn method_assign_args<'tcx>(
-        &self,
-        vcx: &'vir vir::VirCtxt<'tcx>,
-        self_ref: vir::Expr<'vir>,
-        self_new_snap: vir::Expr<'vir>,
-    ) -> &'vir [vir::Expr<'vir>] {
-        assert!(self_ref.ty() == &TypeData::Ref);
-        assert!(self_new_snap.ty() == self.snapshot());
-        let mut args = vec![self_ref];
-        args.extend(self.ty.arg_exprs(vcx));
-        args.push(self_new_snap);
-        vcx.alloc_slice(&args)
-    }
-
     pub fn apply_method_assign<'tcx>(
         &self,
         vcx: &'vir vir::VirCtxt<'tcx>,
         self_ref: vir::Expr<'vir>,
         self_new_snap: vir::Expr<'vir>,
     ) -> vir::Stmt<'vir> {
-        let args = self.method_assign_args(vcx, self_ref, self_new_snap);
-        vcx.alloc(self.generic_predicate.method_assign.apply(vcx, args))
+        assert!(self_ref.ty() == &TypeData::Ref);
+        assert!(self_new_snap.ty() == self.snapshot());
+        let mut args = vec![self_ref];
+        args.extend(self.ty.arg_exprs(vcx));
+        args.push(self_new_snap);
+        vcx.alloc(self.generic_predicate.method_assign.apply(vcx, &args))
     }
 
-    pub fn snapshot(
-        &self
-    ) -> Type<'vir> {
+    pub fn snapshot(&self) -> Type<'vir> {
         self.generic_predicate.snapshot
     }
 
@@ -58,13 +45,7 @@ impl<'vir> PredicateEncOutputRef<'vir> {
         args: &[vir::Expr<'vir>],
         perm: Option<vir::Expr<'vir>>,
     ) -> vir::Expr<'vir> {
-        vcx.mk_predicate_app_expr(
-            self.generic_predicate.ref_to_pred.apply(
-                vcx,
-                args,
-                perm
-            )
-        )
+        vcx.mk_predicate_app_expr(self.generic_predicate.ref_to_pred.apply(vcx, args, perm))
     }
 
     pub fn ref_to_pred_app<'tcx>(
@@ -73,11 +54,7 @@ impl<'vir> PredicateEncOutputRef<'vir> {
         args: &[vir::Expr<'vir>],
         perm: Option<vir::Expr<'vir>>,
     ) -> vir::PredicateApp<'vir> {
-        self.generic_predicate.ref_to_pred.apply(
-            vcx,
-            args,
-            perm
-        )
+        self.generic_predicate.ref_to_pred.apply(vcx, args, perm)
     }
 
     pub fn ref_to_snap<'tcx>(
@@ -88,13 +65,13 @@ impl<'vir> PredicateEncOutputRef<'vir> {
         self.generic_predicate.ref_to_snap.apply(vcx, args)
     }
 
+    /// Arguments to `ref_to_pred` and `ref_to_snap`.
     pub fn ref_to_args<'tcx>(
         &self,
         vcx: &'vir vir::VirCtxt<'tcx>,
         self_ref: vir::Expr<'vir>,
     ) -> &'vir [vir::Expr<'vir>] {
-        self.generic_predicate
-            .ref_to_args(vcx, self.ty, self_ref)
+        self.generic_predicate.ref_to_args(vcx, self.ty, self_ref)
     }
 }
 
@@ -129,7 +106,7 @@ impl TaskEncoder for PredicateEnc {
                 *task_key,
                 PredicateEncOutputRef {
                     generic_predicate,
-                    ty
+                    ty,
                 },
             );
             for arg in args {
