4c227714608044931275b54bfeb5c2a5de481631
Zack Grannan <zgrannan@protonmail.com>
2024-06-26 13:52:20 -0700

    Add some more tests

---
+++ local-testing/rpe/fail/old/verify/loops/tmp_in_loop_condition.rs
@@ -0,0 +1,59 @@
+// ZG: mutable references
+use prusti_contracts::*;
+
+#[trusted]
+fn random() -> u32 {
+    unimplemented!()
+}
+
+fn test1() {
+    let mut tmp = Box::new(Box::new(random()));
+    let tmp_ref = &tmp;
+    let tmp_ref_mut = &mut tmp;
+
+    while {
+        let guard = random() < 55;
+
+        let mut tmp = Box::new(Box::new(random()));
+        let tmp_ref = &tmp;
+        let tmp_ref_mut = &mut tmp;
+
+        guard
+    } {
+        let mut tmp = Box::new(Box::new(random()));
+        let tmp_ref = &tmp;
+        let tmp_ref_mut = &mut tmp;
+    }
+
+    let mut tmp = Box::new(Box::new(random()));
+    let tmp_ref = &tmp;
+    let tmp_ref_mut = &mut tmp;
+    assert!(false); //~ ERROR
+}
+
+fn test2() {
+    let mut tmp = Box::new(Box::new(random()));
+    let tmp_ref = &tmp;
+    let tmp_ref_mut = &mut tmp;
+
+    while {
+        let guard = random() < 55;
+
+        let mut tmp = Box::new(Box::new(random()));
+        let tmp_ref = &tmp;
+        let tmp_ref_mut = &mut tmp;
+
+        guard
+    } {
+        let mut tmp = Box::new(Box::new(random()));
+        let tmp_ref = &tmp;
+        let tmp_ref_mut = &mut tmp;
+        assert!(false); //~ ERROR
+    }
+
+    let mut tmp = Box::new(Box::new(random()));
+    let tmp_ref = &tmp;
+    let tmp_ref_mut = &mut tmp;
+}
+
+fn main() {}
